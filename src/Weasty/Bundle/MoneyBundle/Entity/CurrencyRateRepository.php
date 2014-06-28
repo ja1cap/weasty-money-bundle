dc41797da8ebfb82f087276f5bef1a2f9bbc0df7
ja1cap <ja1cap@me.com>
2014-06-29 01:42:30 +0300

    CurrencyRateRepository sub-class

---
+++ src/Weasty/Bundle/MoneyBundle/Entity/CurrencyRateRepository.php
@@ -0,0 +1,12 @@
+<?php
+namespace Weasty\Bundle\MoneyBundle\Entity;
+
+use Weasty\Money\Entity\CurrencyRateRepository as BaseCurrencyRateRepository;
+
+/**
+ * Class CurrencyRateRepository
+ * @package Weasty\Bundle\MoneyBundle\Entity
+ */
+class CurrencyRateRepository extends BaseCurrencyRateRepository {
+
+} 
