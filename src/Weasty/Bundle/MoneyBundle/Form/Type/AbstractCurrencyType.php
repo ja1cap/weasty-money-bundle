c89c22e50df37fc7942fb9e0fcaa4a5925b2d4b7
ja1cap <ja1cap@me.com>
2016-03-28 22:06:49 +0300

    Improve currency rate form type, add new translations

--- src/Weasty/Bundle/MoneyBundle/Form/Type/AbstractCurrencyType.php
+++ src/Weasty/Bundle/MoneyBundle/Form/Type/AbstractCurrencyType.php
@@ -4,68 +4,72 @@ namespace Weasty\Bundle\MoneyBundle\Form\Type;
 use Symfony\Component\Form\Extension\Core\Type\CurrencyType as BaseCurrencyType;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Weasty\Money\Currency\Code\CurrencyCodeConverter;
+use Weasty\Money\Currency\CurrencyInterface;
 use Weasty\Money\Currency\CurrencyResource;
 
 /**
  * Class AbstractCurrencyType
  * @package Weasty\Bundle\MoneyBundle\Form\Type
  */
-abstract class AbstractCurrencyType extends BaseCurrencyType {
+abstract class AbstractCurrencyType extends BaseCurrencyType
+{
 
 
-    /**
-     * @var \Weasty\Money\Currency\CurrencyResource
-     */
-    protected $currencyResource;
+  /**
+   * @var \Weasty\Money\Currency\CurrencyResource
+   */
+  protected $currencyResource;
 
-    /**
-     * @var \Weasty\Money\Currency\Code\CurrencyCodeConverter
-     */
-    protected $currencyCodeConverter;
+  /**
+   * @var \Weasty\Money\Currency\Code\CurrencyCodeConverter
+   */
+  protected $currencyCodeConverter;
 
-    function __construct(CurrencyResource $currencyResource, CurrencyCodeConverter $currencyCodeConverter)
-    {
-        $this->currencyResource = $currencyResource;
-        $this->currencyCodeConverter = $currencyCodeConverter;
+  function __construct(CurrencyResource $currencyResource, CurrencyCodeConverter $currencyCodeConverter)
+  {
+    $this->currencyResource = $currencyResource;
+    $this->currencyCodeConverter = $currencyCodeConverter;
+  }
+
+  /**
+   * @return string
+   */
+  abstract public function getCodeType();
+
+  /**
+   * @param OptionsResolverInterface $resolver
+   */
+  public function setDefaultOptions(OptionsResolverInterface $resolver)
+  {
+
+    $choices = [];
+
+    foreach ($this->getCurrencyResource()->getCurrencies() as $alphabeticCode => $currency) {
+      if ($currency instanceof CurrencyInterface) {
+        $choices[$currency->getAlphabeticCode()] = $currency;
+      }
     }
 
-    /**
-     * @return string
-     */
-    abstract public function getCodeType();
+    $resolver->setDefaults([
+      'choices' => $choices,
+    ]);
 
-    /**
-     * @param OptionsResolverInterface $resolver
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+  }
 
-        $choices = array();
+  /**
+   * @return \Weasty\Money\Currency\CurrencyResource
+   */
+  public function getCurrencyResource()
+  {
+    return $this->currencyResource;
+  }
 
-        foreach($this->getCurrencyResource()->getCurrencies() as $alphabeticCode => $currency){
-            $choices[$this->getCurrencyCodeConverter()->convert($alphabeticCode, $this->getCodeType(), CurrencyResource::CODE_TYPE_ISO_4217_ALPHABETIC)] = $currency;
-        }
-
-        $resolver->setDefaults(array(
-            'choices' => $choices,
-        ));
-
-    }
-
-    /**
-     * @return \Weasty\Money\Currency\CurrencyResource
-     */
-    public function getCurrencyResource()
-    {
-        return $this->currencyResource;
-    }
-
-    /**
-     * @return \Weasty\Money\Currency\Code\CurrencyCodeConverter
-     */
-    public function getCurrencyCodeConverter()
-    {
-        return $this->currencyCodeConverter;
-    }
+  /**
+   * @return \Weasty\Money\Currency\Code\CurrencyCodeConverter
+   */
+  public function getCurrencyCodeConverter()
+  {
+    return $this->currencyCodeConverter;
+  }
 
 } 
--- src/Weasty/Bundle/MoneyBundle/Form/Type/CurrencyRateType.php
+++ src/Weasty/Bundle/MoneyBundle/Form/Type/CurrencyRateType.php
@@ -4,68 +4,101 @@ namespace Weasty\Bundle\MoneyBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Weasty\Money\Currency\CurrencyResource;
+use Weasty\Money\Entity\CurrencyRate;
 
 /**
  * Class CurrencyRateType
  * @package Weasty\Bundle\MoneyBundle\Form
  */
-class CurrencyRateType extends AbstractType {
+class CurrencyRateType extends AbstractType
+{
 
-    /**
-     * @var \Weasty\Money\Currency\CurrencyResource
-     */
-    protected $currencyResource;
+  /**
+   * @var \Weasty\Money\Currency\CurrencyResource
+   */
+  protected $currencyResource;
 
-    /**
-     * @param \Weasty\Money\Currency\CurrencyResource $currencyResource
-     */
-    function __construct(CurrencyResource $currencyResource)
-    {
-        $this->currencyResource = $currencyResource;
-    }
+  /**
+   * @param \Weasty\Money\Currency\CurrencyResource $currencyResource
+   */
+  function __construct(CurrencyResource $currencyResource)
+  {
+    $this->currencyResource = $currencyResource;
+  }
 
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+  /**
+   * @param FormBuilderInterface $builder
+   * @param array $options
+   */
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
 
-        $builder
-            ->add('sourceAlphabeticCode', 'weasty_money_currency', array(
-                'label' => 'Конвертируемая валюта',
-            ))
-            ->add('destinationAlphabeticCode', 'weasty_money_currency', array(
-                'label' => 'Базовая валюта'
-            ))
-            ->add('rate', 'number', array(
-                'label' => 'Курс конверсии'
-            ))
-        ;
+    $builder
+      ->add('sourceAlphabeticCode', 'weasty_money_currency', [
+        'label' => 'weasty.money.currency_rate.source_alphabetic_code',
+        'translation_domain' => 'WeastyMoneyBundle',
+      ])
+      ->add('destinationAlphabeticCode', 'hidden', [
+        'label' => 'weasty.money.currency_rate.destination_alphabetic_code',
+        'translation_domain' => 'WeastyMoneyBundle',
+        'data' => $this->getCurrencyResource()->getDefaultCurrency(),
+      ])
+      ->add('updatableFromOfficial', 'checkbox', [
+        'label' => 'weasty.money.currency_rate.updatable_form_official',
+        'translation_domain' => 'WeastyMoneyBundle',
+        'attr' => [
+          'ui-switch' => json_encode([
+            'color' => '#1AB394',
+            'size' => 'small',
+          ]),
+          'class' => 'js-switch',
+        ]
+      ])
+      ->add('rate', 'number', [
+        'label' => 'weasty.money.currency_rate.value',
+        'translation_domain' => 'WeastyMoneyBundle',
+      ])
+      ->add('officialOffsetType', 'choice', [
+        'label' => 'weasty.money.official_offset_type',
+        'choices' => [
+          CurrencyRate::OFFICIAL_OFFSET_TYPE_PERCENT => 'weasty.money.official_offset_type.percent',
+          CurrencyRate::OFFICIAL_OFFSET_TYPE_VALUE => 'weasty.money.official_offset_type.value',
+        ],
+        'translation_domain' => 'WeastyMoneyBundle',
+        'data' => CurrencyRate::OFFICIAL_OFFSET_TYPE_PERCENT,
+      ])
+      ->add('officialOffsetPercent', null, [
+        'label' => 'weasty.money.official_offset_type.percent',
+        'translation_domain' => 'WeastyMoneyBundle',
+      ])
+      ->add('officialOffsetValue', null, [
+        'label' => 'weasty.money.official_offset_type.value',
+        'translation_domain' => 'WeastyMoneyBundle',
+      ]);
 
-        $builder
-            ->add('save', 'submit', array(
-                'label' => 'Сохранить',
-            ));
+    $builder
+      ->add('save', 'submit', [
+        'label' => 'Сохранить',
+      ]);
 
-    }
+  }
 
-    /**
-     * Returns the name of this type.
-     *
-     * @return string The name of this type
-     */
-    public function getName()
-    {
-        return 'weasty_money_currency_rate';
-    }
+  /**
+   * Returns the name of this type.
+   *
+   * @return string The name of this type
+   */
+  public function getName()
+  {
+    return 'weasty_money_currency_rate';
+  }
 
-    /**
-     * @return \Weasty\Money\Currency\CurrencyResource
-     */
-    public function getCurrencyResource()
-    {
-        return $this->currencyResource;
-    }
+  /**
+   * @return \Weasty\Money\Currency\CurrencyResource
+   */
+  public function getCurrencyResource()
+  {
+    return $this->currencyResource;
+  }
 
 } 
